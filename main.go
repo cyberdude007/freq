fc60902be382debed82357d140eb868693dc8b7d
cyberdude007 <65081347+cyberdude007@users.noreply.github.com>
2022-03-21 23:39:48 +0530

    Update main.go
    Changed the print statements to grep the positive results from the output

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main(){
 	   			if check_result != false {
 	   				fmt.Println(string(colorRed),"Vulnerable To XSS:", domain,string(colorReset))
 	   			}else{
-	   				fmt.Println(string(colorGreen),"Not Vulnerable To XSS:", domain, string(colorReset))
+	   				fmt.Println(string(colorGreen),"not vulnerable to xss:", domain, string(colorReset))
 	   			}
 
 			}
